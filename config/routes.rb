a3ad6fccfccb25ddd9497356b6bed0af8a1b66bd
Juan Aparicio <apariciojuan30@gmail.com>
2022-04-25 11:58:42 -0300

    set redis as cache store for dev & prod envs

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   mount_devise_token_auth_for 'User', at: 'api/v1/users', controllers: {
