1b6fcf50d898cec57aac5eaf133ef18d738e5106
Juan Aparicio <apariciojuan30@gmail.com>
2022-05-03 11:28:02 -0300

    fix production mailer

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -58,10 +58,12 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rails_api_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: ENV.fetch('SERVER_HOST', 'localhost') }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
