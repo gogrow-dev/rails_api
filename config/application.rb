77bdb2f0b6b8ea64eb107eb5704b0c6051ed9b2d
Juan Aparicio <apariciojuan30@gmail.com>
2023-10-27 15:56:55 -0300

    add missing envs

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,8 @@ module RailsTwitterApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.require_master_key = false
+    config.read_encrypted_secrets = false
   end
 end
