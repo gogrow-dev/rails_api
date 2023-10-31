f010472769966dec62b6f8b8740c5c6662c17067
Juan Aparicio <apariciojuan30@gmail.com>
2023-10-31 16:36:32 +0000

    Minor fixes (#23)
    * ignore .pem files
    
    * fix typo in deploy.staging.yml
    
    * fix corst resource from `/api` to `*`

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ENV.fetch('FRONTEND_URL', 'http://localhost:3000')
-    resource '/api/*',
+    resource '*',
              headers: %w[Authorization],
              methods: :any,
              expose: %w[Authorization],
