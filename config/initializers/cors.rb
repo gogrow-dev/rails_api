1d5267f286a0bcdaa2e31a9595e448fb7d76bb74
Juan Aparicio <apariciojuan30@gmail.com>
2023-11-29 10:15:30 -0300

    fix cors (#28)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,11 +9,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('FRONTEND_URL', 'http://localhost:3000')
+    origins ENV['CORS_ALLOWED_ORIGINS']&.split(',').presence || ENV['FRONTEND_URL'].presence || '*'
     resource '*',
-             headers: %w[Authorization],
-             methods: :any,
-             expose: %w[Authorization],
-             max_age: 600
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             expose: %w[Authorization]
   end
 end
