a7fee1092d07a2ad8c7fd9a6fe1108112bb62d42
Juan Aparicio <apariciojuan30@gmail.com>
2024-02-21 14:08:50 -0300

    enable server timing headers in staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
 require File.expand_path('production.rb', __dir__)
+
+Rails.application.configure do
+  config.server_timing = ENV.fetch('SERVER_TIMING', nil).present?
+end
