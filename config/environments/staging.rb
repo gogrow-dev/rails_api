b5d11e917b1eb730f151654fe3c8889a228b1edb
Juan Aparicio <apariciojuan30@gmail.com>
2024-07-08 10:20:45 -0300

    change staging default log level to debug

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,4 +4,5 @@ require File.expand_path('production.rb', __dir__)
 
 Rails.application.configure do
   config.server_timing = ENV.fetch('SERVER_TIMING', nil).present?
+  config.log_level = ENV.fetch('LOG_LEVEL', :debug).to_sym
 end
