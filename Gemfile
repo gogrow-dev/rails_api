d0e5fedcbb3395517da025af6174e30602552460
Juan Aparicio <apariciojuan30@gmail.com>
2024-08-14 17:02:10 -0300

    update rails to 7.2 (#52)

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby file: '.ruby-version'
 
-gem 'rails', '~> 7.1', '>= 7.1.3.4'
+gem 'rails', '~> 7.2'
 
 gem 'aws-sdk-s3', '~> 1.122'
 gem 'blueprinter', '~> 1.0', '>= 1.0.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_24_152822) do
+ActiveRecord::Schema[7.2].define(version: 2023_11_24_152822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
