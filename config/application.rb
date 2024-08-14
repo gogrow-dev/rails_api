771bf63ec8824381dac382dc4e0403212b7e99cc
Juan Aparicio <apariciojuan30@gmail.com>
2024-08-14 13:59:11 -0300

    update rails to 7.2

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,12 @@ Bundler.require(*Rails.groups)
 module RailsApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks generators])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -44,6 +49,5 @@ module RailsApi
     config.middleware.insert_after(ActionDispatch::Cookies, ActionDispatch::Session::CookieStore)
 
     config.require_master_key = false
-    config.read_encrypted_secrets = false
   end
 end
