4a4d1bb9273e622d0c37e5f58ad027e2436cb15f
Micaela Lorenzo <micaela@gogrow.dev>
2024-09-11 17:09:48 -0300

    add middlewares to autoload lib configuration

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module RailsApi
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks generators])
+    config.autoload_lib(ignore: %w[assets tasks generators middlewares])
 
     # Configuration for the application, engines, and railties goes here.
     #
